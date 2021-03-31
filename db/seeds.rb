347f730caf2f9779150e71c77a14483e22e82029
Marko Glibota <markoglibota@gmail.com>
2021-03-31 13:02:45 +0200

    Create seed with 100 ingredients, 1 cocktail and 1 dose

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+Dose.destroy_all
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)
+
+all_ingredients = []
+
+ingredients["drinks"].each_with_index do |ingredient, index|
+  new_ingredient = Ingredient.new(
+    name: ingredients["drinks"][index]["strIngredient1"]
+    )
+  new_ingredient.save!
+  all_ingredients << new_ingredient
+  puts "#{new_ingredient.name} successfully created"
+end
+
+puts "Finished creating 100 ingredients"
+
+cocktail = Cocktail.new(name: "Mojito")
+cocktail.save!
+puts "Mojito created"
+
+dose = Dose.new(
+  description: "2 cl",
+  cocktail: cocktail,
+  ingredient: all_ingredients.sample
+  )
+
+dose.save!
+puts "One dose created"
+
+
