97d9c1412ad354476cf7a474690b2878daf28125
Marko Glibota <markoglibota@gmail.com>
2021-03-31 11:54:52 +0200

    Add validations for Cocktail, Ingredient, and Dose models

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,2 +1,7 @@
 class Cocktail < ApplicationRecord
+  has_many :doses, dependent: :destroy
+  has_many :ingredients, through: :doses
+  validates :name, presence: true, uniqueness: true
+
+  accepts_nested_attributes_for :ingredients, :doses
 end
