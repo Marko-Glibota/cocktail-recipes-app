26453c81f8fb94bb87c44a66e6749c05382cf2d6
Marko Glibota <markoglibota@gmail.com>
2021-04-01 12:03:58 +0200

    Fix dose destroy method and add dose delete button on cocktail show

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,13 +1,13 @@
 class DosesController < ApplicationController
+  before_action :set_cocktail
+
   def new
     @dose = Dose.new
-    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose.cocktail = @cocktail
     @ingredients = Ingredient.all
   end
 
   def create
-    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
     if @dose.save
@@ -17,7 +17,17 @@ class DosesController < ApplicationController
     end
   end
 
+  def destroy
+    @dose = Dose.find_by(cocktail_id: @cocktail)
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
   private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
 
   def dose_params
     params.require(:dose).permit(:description, :ingredient_id)
